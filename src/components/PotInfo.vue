374aea9ea2647ecd67e712a3b4aceeb9cf1d1250
Yang hyeji <1999lover@naver.com>
2022-10-01 17:24:09 +0900

    css 코드 적용

--- src/components/PotInfo.vue
+++ src/components/PotInfo.vue
@@ -1,11 +1,43 @@
 <template>
   <v-container>
-    <v-btn @click="$router.push('/signup')">회원가입</v-btn><br>
-    <v-btn @click="$router.push('/login')">로그인</v-btn>
+ <input type="hidden" id="anPageName" name="page" value="annaecang1" />
+    <div class="container-center-horizontal">
+      <div class="annaecang1 screen">
+        <div class="flex-row">
+          <img class="icon" src="img/icon-1@2x.svg" alt="Icon" />
+          <div class="skip">Skip</div>
+        </div>
+        <div class="text-28">배달팟의 주인장과 팟원이 되어보세요!</div>
+        <div class="group-389">
+          <div class="ellipse-container">
+            <img class="ellipse-5" src="img/ellipse-5@2x.svg" alt="Ellipse 5" />
+            <img class="ellipse-12" src="img/ellipse-12@2x.png" alt="Ellipse 12" />
+            <img class="ellipse-6" src="img/ellipse-6@2x.png" alt="Ellipse 6" />
+            <img class="ellipse-7" src="img/ellipse-7@2x.svg" alt="Ellipse 7" />
+            <img class="ellipse-8" src="img/ellipse-8@2x.svg" alt="Ellipse 8" />
+            <img class="ellipse-10" src="img/ellipse-10@2x.svg" alt="Ellipse 10" />
+            <img class="ellipse-11" src="img/ellipse-11@2x.png" alt="Ellipse 11" />
+            <img class="ellipse-9" src="img/ellipse-9@2x.svg" alt="Ellipse 9" />
+          </div>
+        </div>
+        <p class="text-29 valign-text-middle">
+          ‘주인장&#39; : 배달팟을 모으기 위해 팟을 개설한 이용자<br />‘팟원’ : 근거리의 주인장이 개설한 배달팟에 참여한
+          배달팟의 일원
+        </p>
+        <div class="group-458">
+          <a href="/signup">
+            <div class="button"><div class="create-an-account dmsans-bold-white-14px">회원가입</div></div></a
+          ><a href="/login">
+            <div class="button-1"><div class="create-an-account-1">로그인</div></div></a
+          >
+        </div>
+      </div>
+    </div>    
   </v-container>
 </template>
 
 <script>
+import '../../public/css/annaecang1.css'
 export default {
 }
 </script>
--- src/components/PotSignup.vue
+++ src/components/PotSignup.vue
@@ -2,6 +2,80 @@
 <v-app>
   <v-main>
       <v-container>
+        <input type="hidden" id="anPageName" name="page" value="screen2" />
+    <div class="container-center-horizontal">
+      <div class="screen2 screen">
+        <div class="flex-col">
+          <img class="icon" src="img/icon-4@2x.svg" alt="Icon" />
+          <div class="text-61 dmsans-bold-eerie-black-24px">회원가입</div>
+          <p class="text-62 dmsans-normal-masala-14px">본인의 이름, 전화번호, 이메일 정보를 수집합니다.</p>
+          <v-form ref="formFirst">
+            <div class="form-1">
+              <!-- <div class="title dmsans-normal-masala-12px">이름</div> -->
+              <!-- <div class="overlap-group"><div class="place-holder dmsans-normal-silver-chalice-14px">김배달</div></div> -->
+              <v-col>
+                <h6>이름</h6>
+                <v-text-field v-model = "name" label = "name" :rules="[value => !!value || '이름을 입력 하세요.']" placeholder="김배달"> </v-text-field>
+              </v-col>
+            </div>
+            <div class="form">
+              <!-- <div class="title dmsans-normal-masala-12px">E-mail</div> -->
+              <!-- <div class="overlap-group">
+                <div class="place-holder dmsans-normal-silver-chalice-14px">baedalpot@gmail.com</div>
+            </div> -->
+              <v-col>
+                <h6>e-mail주소</h6>
+                <v-text-field v-model = "email" label ="email" :rules="[value => !!value || '이메일을 입력 하세요.']" placeholder="baedalpot@gmail.com"></v-text-field>
+              </v-col>
+            </div>
+            <div class="form">
+              <!-- <div class="title dmsans-normal-masala-12px">휴대전화번호</div>
+              <div class="overlap-group">
+                <div class="place-holder dmsans-normal-silver-chalice-14px">010-1234-5678</div>
+              </div> -->
+              <v-col>
+                <h6> 휴대전화 번호</h6>
+                <h6> -포함해서 입력하세요 </h6>
+                <v-text-field v-model = "phonenum" label = "phonenum" :rules="[value => !!value || '전화번호를 입력 하세요.']" placeholder="010-1234-5678"></v-text-field>
+                <div id = "recaptcha-container" ></div>
+                <v-btn @click="setKeyIdentifier()">인증받기</v-btn>
+              </v-col>
+            </div>
+          </v-form>
+          <div class="form">
+            <!-- <div class="title dmsans-normal-masala-12px">인증번호</div>
+            <div class="overlap-group">
+              <p class="place-holder-1 dmsans-normal-silver-chalice-12px">
+                휴대전화로 발송된 인증번호 4자리를 입력하세요
+              </p>
+            </div> -->
+            <v-form ref="formSecond">
+              <div class = "IdentifierNum">
+                <v-col>
+                  <h6>인증번호 입력</h6>
+                  <v-text-field v-model = "identifiernum" label = "identifier" :rules="[value => !!value || '인증번호를 입력 하세요.']" placeholder="휴대전화로 발송된 인증번호 4자리를 입력하세요"></v-text-field>
+                </v-col>
+                <v-checkbox label = "개인정보수집, 위치정보수집에 동의합니다." v-model="agree"> </v-checkbox>
+                <v-btn @click="SignIn()">회원가입</v-btn>
+              </div>
+            </v-form>
+          </div>
+
+          <!-- <div class="checkbox"><div class="type-here">개인정보수집, 위치정보수집에 동의합니다.</div></div> -->
+          <div class="group-458">
+            <div class="button" @click="SignIn()"><div class="create-an-account dmsans-bold-white-14px">회원가입</div></div>
+            <a href="/login">
+              <div class="button-1"><div class="create-an-account-1">기존 계정으로 로그인하기</div></div></a
+            >
+          </div>
+        </div>
+        <img class="job-finder-app-1" src="img/job-finder-app-1@1x.png" alt="job finder app 1" />
+      </div>
+    </div>
+
+
+
+
         <v-form ref="formFirst">
           <div class =  "Name">
             <v-col>
@@ -41,6 +115,7 @@
 </template>
 
 <script>
+import "../../public/css/screen2.css"
 import {
   getAuth,
   signOut,
