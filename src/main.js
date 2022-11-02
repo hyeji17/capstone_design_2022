dd902a4b8975b0d88bc26b3599d1f8ba2a5c3049
Yang hyeji <1999lover@naver.com>
2022-11-02 11:21:28 +0900

    pwa 부분 수정중

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
